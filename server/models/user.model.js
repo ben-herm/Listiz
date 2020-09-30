586378a3cdf1d942163a29086e70f766c947cc90
Ben Herman <ben.h@onoapps.com>
2020-09-30 10:47:08 +0300

    s

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,18 +1,18 @@
-const bcrypt = require("bcrypt");
-const crypto = require("crypto");
-const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
+const crypto = require('crypto')
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const userSchema = new Schema(
   {
     email: { type: String, unique: true },
     profile: {
-      name: String,
-      gender: String,
-      location: String,
-      website: String,
-      picture: String
+      name: { type: String, unique: true, required: true }
+      // gender: String,
+      // location: String,
+      // website: String,
+      // picture: String
     },
     passwordResetToken: String,
     passwordResetExpires: Date,
@@ -26,50 +26,50 @@ const userSchema = new Schema(
   {
     timestamps: true
   }
-);
+)
 
-userSchema.pre("save", function save(next) {
-  const user = this;
-  if (!user.isModified("password")) {
-    return next();
+userSchema.pre('save', function save (next) {
+  const user = this
+  if (!user.isModified('password')) {
+    return next()
   }
   bcrypt.genSalt(10, (err, salt) => {
     if (err) {
-      return next(err);
+      return next(err)
     }
     bcrypt.hash(user.password, salt, (err, hash) => {
       if (err) {
-        return next(err);
+        return next(err)
       }
-      user.password = hash;
-      next();
-    });
-  });
-});
+      user.password = hash
+      next()
+    })
+  })
+})
 
-userSchema.methods.comparePassword = function comparePassword(
+userSchema.methods.comparePassword = function comparePassword (
   candidatePassword,
   cb
 ) {
   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    cb(err, isMatch);
-  });
-};
+    cb(err, isMatch)
+  })
+}
 
-userSchema.methods.gravatar = function gravatar(size) {
+userSchema.methods.gravatar = function gravatar (size) {
   if (!size) {
-    size = 200;
+    size = 200
   }
   if (!this.email) {
-    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+    return `https://gravatar.com/avatar/?s=${size}&d=retro`
   }
   const md5 = crypto
-    .createHash("md5")
+    .createHash('md5')
     .update(this.email)
-    .digest("hex");
-  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-};
+    .digest('hex')
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`
+}
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,132 +1,132 @@
-const router = require("express").Router();
-const { promisify } = require("util");
-let User = require("../models/user.model");
-const crypto = require("crypto");
-const nodemailer = require("nodemailer");
-const passport = require("passport");
+const router = require('express').Router()
+const { promisify } = require('util')
+let User = require('../models/user.model')
+const crypto = require('crypto')
+const nodemailer = require('nodemailer')
+const passport = require('passport')
 // const _ = require('lodash');
-const validator = require("validator");
-const mailChecker = require("mailchecker");
-const randomBytesAsync = promisify(crypto.randomBytes);
+const validator = require('validator')
+const mailChecker = require('mailchecker')
+const randomBytesAsync = promisify(crypto.randomBytes)
 
-const REGISTRATION_SUCCESS = "REGISTRATION_SUCCESS";
-const REGISTRATION_FAILURE = "REGISTRATION_FAILURE";
+const REGISTRATION_SUCCESS = 'REGISTRATION_SUCCESS'
+const REGISTRATION_FAILURE = 'REGISTRATION_FAILURE'
 // const User = require('../models/User');
 
 exports.postLogin = (req, res, next) => {
-  const validationErrors = [];
+  const validationErrors = []
   if (!validator.isEmail(req.body.email))
-    validationErrors.push({ msg: "Please enter a valid email address." });
+    validationErrors.push({ msg: 'Please enter a valid email address.' })
   if (validator.isEmpty(req.body.password))
-    validationErrors.push({ msg: "Password cannot be blank." });
+    validationErrors.push({ msg: 'Password cannot be blank.' })
 
   if (validationErrors.length) {
-    req.flash("errors", validationErrors);
-    return res.redirect("/login");
+    req.flash('errors', validationErrors)
+    return res.redirect('/login')
   }
   req.body.email = validator.normalizeEmail(req.body.email, {
     gmail_remove_dots: false
-  });
+  })
 
-  passport.authenticate("local", (err, user, info) => {
+  passport.authenticate('local', (err, user, info) => {
     if (err) {
-      return next(err);
+      return next(err)
     }
     if (!user) {
-      req.flash("errors", info);
-      return res.redirect("/login");
+      req.flash('errors', info)
+      return res.redirect('/login')
     }
     req.logIn(user, err => {
       if (err) {
-        return next(err);
+        return next(err)
       }
-      req.flash("success", { msg: "Success! You are logged in." });
-      res.redirect(req.session.returnTo || "/");
-    });
-  })(req, res, next);
-};
+      req.flash('success', { msg: 'Success! You are logged in.' })
+      res.redirect(req.session.returnTo || '/')
+    })
+  })(req, res, next)
+}
 
 exports.postSignup = (req, res, next) => {
-  const validationErrors = [];
+  const validationErrors = []
 
   if (!validator.isEmail(req.body.email)) {
     validationErrors.push({
-      emailError: "Please enter a valid email address."
-    });
+      emailError: 'Please enter a valid email address.'
+    })
   }
 
   if (validator.isEmpty(req.body.userName)) {
     validationErrors.push({
-      nameError: "Please choose a userName"
-    });
+      nameError: 'Please choose a userName'
+    })
   } else if (!validator.isLength(req.body.userName, { min: 3 })) {
     validationErrors.push({
-      nameError: "UserName must be at least 3 characters long"
-    });
+      nameError: 'UserName must be at least 3 characters long'
+    })
   }
 
   if (!validator.isLength(req.body.password, { min: 8 })) {
     validationErrors.push({
-      passwordError: "Password must be at least 8 characters long"
-    });
+      passwordError: 'Password must be at least 8 characters long'
+    })
   }
 
   //   if (req.body.password !== req.body.confirmPassword)
   //     validationErrors.push({ msg: "Passwords do not match" });
   if (validationErrors.length >= 2) {
-    console.log("validationErrors.length2222");
-    req.flash("errors", validationErrors);
-    return res.status(400).send({ error: validationErrors });
+    console.log('validationErrors.length2222')
+    req.flash('errors', validationErrors)
+    return res.status(400).send({ error: validationErrors })
   } else if (validationErrors.length) {
-    console.log("validationErrors.length");
-    return res.status(400).send({ error: validationErrors[0] });
+    console.log('validationErrors.length')
+    return res.status(400).send({ error: validationErrors[0] })
   }
   req.body.email = validator.normalizeEmail(req.body.email, {
     gmail_remove_dots: false
-  });
+  })
   const user = new User({
     profile: {
       name: req.body.userName
     },
     email: req.body.email,
     password: req.body.password
-  });
-  console.log("req.body.email", req.body.email);
+  })
+  console.log('req.body.email', req.body.email)
   User.findOne({ email: req.body.email }, (err, existingUser) => {
     if (err) {
-      console.log("err3: ", err);
-      return next(err);
+      console.log('err3: ', err)
+      return next(err)
     }
     if (existingUser) {
-      console.log("existingUser", existingUser);
-      req.flash("errors", {
-        msg: "Account with that email address already exists."
-      });
+      console.log('existingUser', existingUser)
+      req.flash('errors', {
+        msg: 'Account with that email address already exists.'
+      })
       // 409 code for conflict with dataBase resource.
       return res.status().send(409, {
-        error: "Account with that email address already exists."
-      });
+        error: 'Account with that email address already exists.'
+      })
     }
     user.save(err => {
       if (err) {
-        console.log("err: ", err);
-        return next(err);
+        console.log('err: ', err)
+        return next(err)
       }
       req.logIn(user, err => {
         if (err) {
-          console.log("err2: ", err);
-          return next(err);
+          console.log('err2: ', err)
+          return next(err)
         }
-        console.log("log in");
+        console.log('log in')
         return res.send({
           message: REGISTRATION_SUCCESS,
           userObject: req.body,
-          returnCode: "1"
-        });
-      });
-    });
-  });
-};
+          returnCode: '1'
+        })
+      })
+    })
+  })
+}
 
 /**
  * GET /account/verify
@@ -134,62 +134,62 @@ exports.postSignup = (req, res, next) => {
  */
 exports.getVerifyEmail = (req, res, next) => {
   if (req.user.emailVerified) {
-    req.flash("info", { msg: "The email address has been verified." });
-    return res.redirect("/account");
+    req.flash('info', { msg: 'The email address has been verified.' })
+    return res.redirect('/account')
   }
 
   if (!mailChecker.isValid(req.user.email)) {
-    req.flash("errors", {
+    req.flash('errors', {
       msg:
-        "The email address is invalid or disposable and can not be verified.  Please update your email address and try again."
-    });
-    return res.redirect("/account");
+        'The email address is invalid or disposable and can not be verified.  Please update your email address and try again.'
+    })
+    return res.redirect('/account')
   }
 
   const createRandomToken = randomBytesAsync(16).then(buf =>
-    buf.toString("hex")
-  );
+    buf.toString('hex')
+  )
 
   const setRandomToken = token => {
     User.findOne({ email: req.user.email }).then(user => {
-      user.emailVerificationToken = token;
-      user = user.save();
-    });
-    return token;
-  };
+      user.emailVerificationToken = token
+      user = user.save()
+    })
+    return token
+  }
 
   const sendVerifyEmail = token => {
     let transporter = nodemailer.createTransport({
-      service: "SendGrid",
+      service: 'SendGrid',
       auth: {
         user: process.env.SENDGRID_USER,
         pass: process.env.SENDGRID_PASSWORD
       }
-    });
+    })
     const mailOptions = {
       to: req.user.email,
-      from: "hackathon@starter.com",
-      subject: "Please verify your email address on Hackathon Starter",
+      from: 'hackathon@starter.com',
+      subject: 'Please verify your email address on Hackathon Starter',
       text: `Thank you for registering with hackathon-starter.\n\n
         This verify your email address please click on the following link, or paste this into your browser:\n\n
         http://${req.headers.host}/account/verify/${token}\n\n
         \n\n
         Thank you!`
-    };
+    }
     return transporter
       .sendMail(mailOptions)
       .then(() => {
-        req.flash("info", {
+        req.flash('info', {
           msg: `An e-mail has been sent to ${req.user.email} with further instructions.`
-        });
+        })
       })
       .catch(err => {
-        if (err.message === "self signed certificate in certificate chain") {
+        if (err.message === 'self signed certificate in certificate chain') {
           console.log(
-            "WARNING: Self signed certificate in certificate chain. Retrying with the self signed certificate. Use a valid certificate if in production."
-          );
+            'WARNING: Self signed certificate in certificate chain. Retrying with the self signed certificate. Use a valid certificate if in production.'
+          )
           transporter = nodemailer.createTransport({
-            service: "SendGrid",
+            service: 'SendGrid',
             auth: {
               user: process.env.SENDGRID_USER,
               pass: process.env.SENDGRID_PASSWORD
@@ -197,71 +197,71 @@ exports.getVerifyEmail = (req, res, next) => {
             tls: {
               rejectUnauthorized: false
             }
-          });
+          })
           return transporter.sendMail(mailOptions).then(() => {
-            req.flash("info", {
+            req.flash('info', {
               msg: `An e-mail has been sent to ${req.user.email} with further instructions.`
-            });
-          });
+            })
+          })
         }
         console.log(
-          "ERROR: Could not send verifyEmail email after security downgrade.\n",
+          'ERROR: Could not send verifyEmail email after security downgrade.\n',
           err
-        );
-        req.flash("errors", {
+        )
+        req.flash('errors', {
           msg:
-            "Error sending the email verification message. Please try again shortly."
-        });
-        return err;
-      });
-  };
+            'Error sending the email verification message. Please try again shortly.'
+        })
+        return err
+      })
+  }
 
   createRandomToken
     .then(setRandomToken)
     .then(sendVerifyEmail)
-    .then(() => res.redirect("/account"))
-    .catch(next);
-};
+    .then(() => res.redirect('/account'))
+    .catch(next)
+}
 
 exports.logout = (req, res) => {
-  req.logout();
+  req.logout()
   req.session.destroy(err => {
     if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
-    res.redirect("/");
-  });
-};
+      console.log('Error : Failed to destroy the session during logout.', err)
+    req.user = null
+    res.redirect('/')
+  })
+}
 
 exports.postLogin = (req, res, next) => {
-  const validationErrors = [];
+  const validationErrors = []
   if (!validator.isEmail(req.body.email))
-    validationErrors.push({ msg: "Please enter a valid email address." });
+    validationErrors.push({ msg: 'Please enter a valid email address.' })
   if (validator.isEmpty(req.body.password))
-    validationErrors.push({ msg: "Password cannot be blank." });
+    validationErrors.push({ msg: 'Password cannot be blank.' })
 
   if (validationErrors.length) {
-    req.flash("errors", validationErrors);
-    return res.redirect("/login");
+    req.flash('errors', validationErrors)
+    return res.redirect('/login')
   }
   req.body.email = validator.normalizeEmail(req.body.email, {
     gmail_remove_dots: false
-  });
+  })
 
-  passport.authenticate("local", (err, user, info) => {
+  passport.authenticate('local', (err, user, info) => {
     if (err) {
-      return next(err);
+      return next(err)
     }
     if (!user) {
-      req.flash("errors", info);
-      return res.redirect("/login");
+      req.flash('errors', info)
+      return res.redirect('/login')
     }
     req.logIn(user, err => {
       if (err) {
-        return next(err);
+        return next(err)
       }
-      req.flash("success", { msg: "Success! You are logged in." });
-      res.redirect(req.session.returnTo || "/");
-    });
-  })(req, res, next);
-};
+      req.flash('success', { msg: 'Success! You are logged in.' })
+      res.redirect(req.session.returnTo || '/')
+    })
+  })(req, res, next)
+}
